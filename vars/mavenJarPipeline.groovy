2e0d7b07a3e569b6dd1fb0744b79b9181ef47090
Ravi Malvia <malviaravi0000@gmail.com>
2023-02-20 11:23:56 +0530

    sd

--- vars/mavenJarPipeline.groovy
+++ vars/mavenJarPipeline.groovy
@@ -23,18 +23,20 @@ def call(String githubUrl ){
 			}
 			stage("Build Image"){
 				steps{
-				sh 'ls'
-				sh 'pwd'
-				sh 'whoami'
-				sh 'docker build -t ravimalvia/java_app:v1 .'
+				//sh 'ls'
+				//sh 'pwd'
+				//sh 'whoami'
+				sh 'docker build -t ravimalvia/java_app:${BUILD_NUMBER} .'
 				
 				}
 			}
-			stage("Dockerhub login"){
+			stage("Dockerhub login and image push to dockerhub"){
 				steps{
 				sh 'echo $DOCKERHUB_CRED_PSW | docker login -u $DOCKERHUB_CRED_USR --password-stdin'
+				sh 'docker push ravimalvia/java_app:${BUILD_NUMBER}'
 				}
 			}
+			
 		}
 	
 	}
